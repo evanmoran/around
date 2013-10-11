f476dd62978aa417df990e01ec123d313d53f4fd
Evan Moran <evan@evanmoran.com>
2013-10-11 10:02:39 -0400

    Updating source

--- src/around.js
+++ src/around.js
@@ -54,7 +54,6 @@
 // Examples
 // -------------------------------------------------------------
 
-// Globals are bad
 (function () {
 
   // Establish the root object, `window` in the browser, or `global` on the server.
@@ -75,6 +74,10 @@
 
   var arrayify = function(v){
   	return v == null ? [] : isArray(v) ? v : [v];
+  };
+
+  var toRealArray = function(args){
+		return Array.prototype.slice.call(args);
   };
 
   // CommonJS / Node support
@@ -117,76 +120,97 @@
 	    var skipSelf = false;
 	    var fnOut = function()
 	    {
+	    	// console.log('\n\n\n')
 
 				// Call before or pass through args
-				var args = arguments;
-				var beforeOut = args;
-				console.log('args (before): ', args)
+				var args = toRealArray(arguments);  // What should be passed if a function is passed
+				var out = undefined; 								// What should be returned if done
+				var selfOut = undefined;
+
+				// console.log('args (start): ', args)
+
 				if (typeof fnBefore === "function"){
 					// Pass through this
-					console.log('before called')
-					beforeOut = fnBefore.apply(fnSelf, args);
+					// console.log('before called')
+					var beforeOut = fnBefore.apply(fnSelf, args);
+					// Before only overrides arguments if it returns something
+					if(beforeOut != null){
+						args = out = beforeOut;
+					}
 				}
-				console.log('beforeOut: ', beforeOut);
-
+				// console.log('args (before): ', args)
+				// console.log('out (before): ', out)
 				// Skip fnSelf if breaker was found
-				if(beforeOut === around.breaker || beforeOut[0] == around.breaker){
+				if(out === around.breaker || (out != null && out[0] == around.breaker))
+				{
+					// console.log('breaker found"')
 					skipSelf = true;
-					// Remove breaker from list if necessary
-					if(isArray(beforeOut))
-						beforeOut.shift();
+					// Remove breaker from out
+					if(isArray(out))
+						out.shift();
+					else
+						out = undefined;
 				}
 
 				// Setup args for next function
-				args = arrayify(beforeOut);
+				args = arrayify(out);
 
-				console.log('args (self): ', args)
+				// console.log('args (self): ', args)
 
-		    var selfOut;
-				var flagExceptionCaught = false;
-				if (!skipSelf)
+				var flagException = false;
+
+				// Finish fully if there is no self or after
+				// Finish with self if there is no after.
+				// (prevents unecessary try/catch calls)
+				if(typeof fnAfter !== 'function')
 				{
-					// Finish with self if there is no after. This also
-					// prevents unecessary try/catch calls
-					if(typeof fnAfter !== 'function') {
-						console.log('self called with return')
-						return fnSelf.apply(fnSelf, args);
+					// console.log('after not found')
+					// Finish immediately if there is no self
+					if (skipSelf){
+						// console.log('exiting immediately (no self or after): ', out)
+						return out;
 					}
-					else
-					{
-						// Exceptions are caught and passed to the after functon just like return
-						// values so fnAfter can modify these as well.
-						try {
-							console.log('self called')
-							selfOut = fnSelf.apply(fnSelf, args);
-						}
-						catch(selfException) {
-							selfOut = selfException;
-							flagExceptionCaught = true;
-						}
-						args = arrayify(selfOut);
-					}
+					// console.log('self called with return')
+					return fnSelf.apply(fnSelf, args);
 				}
 
-				console.log('args (after): ', args)
+				// Exceptions are caught and passed to the after functon just like return
+				// values so fnAfter can modify these as well.
+				try {
+					// console.log('self called')
+					out = fnSelf.apply(fnSelf, args);
+					// console.log('out: ', out)
+				}
+				catch(e) {
+					// console.log('exception caught')
+					out = e;
+					flagException = true;
+				}
+				args = arrayify(out);
 
+				// console.log('out (after): ', out)
+				// console.log('args (after): ', args)
 
 				// Call after
-				var afterOut = undefined;
-				if (typeof fnAfter === "function"){
-					console.log('after called')
-					afterOut = fnAfter.apply(fnSelf, args);
+				if (typeof fnAfter === "function")
+				{
+					// console.log('after called')
+					var afterOut = fnAfter.apply(fnSelf, args);
+
+					// After can overwrite exceptions
+					if (afterOut !== undefined){
+						out = afterOut;
+						flagException = false;
+					}
 				}
-				// Return result of `fnAfter` if it returned something
-				// Note that `fnAfter` can rethrow itself so we don't have to
-				if (afterOut !== undefined)
-					return afterOut;
+
+				// console.log('out (end): ', out)
 
 				// Rethrow fnSelf's exception if it was caught. This way the behavior
 				// of the original function is changed only if fnAfter changes it
-				if (flagExceptionCaught)
-					throw(selfOut);
-				return selfOut;
+				if (flagException)
+					throw(out);
+				return out;
 			};
 			return fnOut;
 		}
